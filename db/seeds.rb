91ec9b79c4009016e71ac95897a52c5504fbd385
anees1998 <aneesyasin213@gmail.com>
2022-11-16 13:47:39 +0000

    portfolio functionality added

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,31 +16,31 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-10.times do |blog|
-    Blog.create!(
-        Title: "blogNo #{blog}",
-        Body: "random body"
-    )
-end
+# 10.times do |blog|
+#     Blog.create!(
+#         Title: "blogNo #{blog}",
+#         Body: "random body"
+#     )
+# end
 
-puts "5 blog posts created"
+# puts "5 blog posts created"
 
-  10.times do |skill|
-    Skill.create!(
-      title: "Rails #{skill}",
-      percent_utilized: 15
-    )
-  end
+#   10.times do |skill|
+#     Skill.create!(
+#       title: "Rails #{skill}",
+#       percent_utilized: 15
+#     )
+#   end
 
-  puts "5 skills created"
+#   puts "5 skills created"
 
   9.times do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}",
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      main_image: "http://placehold.it/600x400",
-      thumb_image: "http://placehold.it/350x200"
-    ) 
+      main_image: "https://via.placeholder.com/300",
+      thumb_image: "https://via.placeholder.com/150"
+          ) 
   end
 
   puts "9 portfolio items created"
