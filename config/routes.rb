64e87c535804995b383f6ab0f928c434a22af34a
anees1998 <aneesyasin213@gmail.com>
2022-11-17 23:23:20 +0500

    toggle status with custom routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   #get 'pages/about'
   get 'pages/contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
